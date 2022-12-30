1f1fc0a74a447a10c8ff817e9077d8b2ad34543f
scaric-pmfst <scaric@pmfst.hr>
2022-12-30 10:19:58 +0100

    Korisničko Sučelje
    - Dodan prozor GlavniMeni
    - Dodano dugme za pokretanje BlackJacka, Rouletta, te za kupovinu i prodaju čipova
    - Dodani događaji kad se pritisne dugme za kupit i dugme za prodat čipove

---
+++ Casino/GlavniMeni.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Casino
+{
+    /// <summary>
+    /// Interaction logic for GlavniMeni.xaml
+    /// </summary>
+    public partial class GlavniMeni : Window
+    {
+        public GlavniMeni()
+        {
+            InitializeComponent();
+        }
+
+        private void IgrajBlackJack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void IgrajRoulette_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void KupiChipove_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ProdajChipove_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
