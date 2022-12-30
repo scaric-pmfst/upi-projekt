24221144aafc62b4965385b18e868832f15a20f5
MateaDR <mdrazin@pmfst.hr>
2022-12-30 18:51:22 +0100

    Roulette
    - Dodana metoda DodavanjeBrojeva
    - Dodan .txt datoteka sa svim brojevima i njihovim bojama (za referencu)
    - Dodane globalne varijable potrebne za igru

--- Casino/Roulette.xaml.cs
+++ Casino/Roulette.xaml.cs
@@ -19,11 +19,16 @@ namespace Casino
     /// </summary>
     public partial class Roulette : Window
     {
+        Dictionary<int, string> brojevi = new Dictionary<int, string>();
+        Dictionary<string, double> ulog = new Dictionary<string, double>();
+        List<string> ulozeneStvari = new List<string>();
+        double ulozeniNovac;
         double TrenutniChipovi;
         public Roulette()
         {
             InitializeComponent();
             TrenutnoStanje();
+            DodavanjeBrojeva();
         }
 
         //Metoda pomoću koje dohvaćamo trenutno stanje čipova
@@ -31,6 +36,81 @@ namespace Casino
         {
             Stanje.Text = ((GlavniMeni)Application.Current.MainWindow).Chipovi.Text;
             TrenutniChipovi = double.Parse(((GlavniMeni)Application.Current.MainWindow).Chipovi.Text.Replace("€", ""));
+        }
+
+        //Metode
+        //Metoda pomoću koje dodajemo brojeve i njihove boje u Dictionary
+        private void DodavanjeBrojeva()
+        {
+            Console.WriteLine("Metoda DodavanjeBrojeva.");
+            for (int i = 0; i < 11; i++)
+            {
+                if (i == 0)
+                {
+                    brojevi.Add(0, "Zelena");
+                }
+                else if (i == 1)
+                {
+                    brojevi.Add(1, "Crvena");
+                }
+                else if (i % 2 != 0)
+                {
+                    brojevi.Add(i, "Crvena");
+                }
+                else
+                {
+                    brojevi.Add(i, "Crna");
+                }
+            }
+            for (int i = 11; i < 20; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    brojevi.Add(i, "Crna");
+                }
+                else
+                {
+                    brojevi.Add(i, "Crvena");
+                }
+            }
+            for (int i = 20; i < 29; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    brojevi.Add(i, "Crvena");
+                }
+                else
+                {
+                    brojevi.Add(i, "Crna");
+                }
+            }
+            for (int i = 29; i < 37; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    brojevi.Add(i, "Crna");
+                }
+                else
+                {
+                    brojevi.Add(i, "Crvena");
+                }
+            }
+            foreach (int broj in brojevi.Keys)
+            {
+                Console.WriteLine(broj + " " + brojevi[broj]);
+            }
+            Console.WriteLine("Metoda DodavanjeBrojeva izvršena.");
+        }
+
+
+        private void Igraj_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Dugme_Ulog_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
