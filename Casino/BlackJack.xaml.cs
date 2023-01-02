97c90452bf2f8733183f0d08d21fd187f0ff1491
mmaks21 <mzupa@pmfst.hr>
2023-01-02 18:48:58 +0100

    BlackJack
    - Dodana klasa Karte sa bojama i brojevima
    - Dodane globalne varijable potrebne za igru
    - Dodana metoda OcistiRuke pomoću koje cistimo ruke nakon odigrane partije
    - Dodana metoda NapraviDeck pomoću koje pravimo novi dek karata
    - Dodana metoda PocetnaRuka pomoću koje se dijele pocetne karte

--- Casino/BlackJack.xaml.cs
+++ Casino/BlackJack.xaml.cs
@@ -19,7 +19,16 @@ namespace Casino
     /// </summary>
     public partial class BlackJack : Window
     {
+        //Globalne varijable
+        Karte karte = new Karte();
+        Random r = new Random();
+        List<string> Deck = new List<string>();
+        List<string> RukaIgraca = new List<string>();
+        List<string> RukaProtivnika = new List<string>();
+        int JakostRukeIgraca, ulozeniNovac;
+        bool IgrajBlackJack;
         double TrenutniChipovi;
+
         public BlackJack()
         {
             InitializeComponent();
@@ -32,5 +41,96 @@ namespace Casino
             Stanje.Text = ((GlavniMeni)Application.Current.MainWindow).Chipovi.Text;
             TrenutniChipovi = double.Parse(((GlavniMeni)Application.Current.MainWindow).Chipovi.Text.Replace("€", ""));
         }
+        //Metode
+
+        //Metoda pomoću koje cistimo ruke igrača i protivnika
+        private void OcistiRuke()
+        {
+            Console.WriteLine("Metoda OcistiRuke");
+            RukaProtivnika.Clear();
+            RukaIgraca.Clear();
+            ProtivnikovaRuka.Text = "";
+            IgracevaRuka.Text = "";
+            JakostRukeIgraca = 0;
+            Console.WriteLine("Metoda OcistiRuke izvrsena");
+        }
+
+        //Metoda pomoću koje pravimo novi dek
+        private void NapraviDeck()
+        {
+            Console.WriteLine("NapraviDeck Metoda");
+            Deck.Clear();
+            foreach (char boja in karte.Boje)
+            {
+                foreach (string broj in karte.Brojevi)
+                {
+                    Console.WriteLine(broj + boja);
+                    Deck.Add(broj + boja);
+                }
+            }
+            Console.WriteLine("Metoda NapraviDeck izvrsena.");
+        }
+
+        //Metoda pomoću koje dijelimo početne karte igraču i protivniku
+        private void PocetnaRuka()
+        {
+            Console.WriteLine("Metoda PocetnaRuka");
+            for (int i = 0; i < 3; i++)
+            {
+                List<string> tempDeck = Deck;
+                string izvucenaKarta = tempDeck[r.Next(0, tempDeck.Count)];
+                tempDeck.Remove(izvucenaKarta);
+                if (i != 1)
+                {
+                    Console.WriteLine("Igraceva Karta: " + izvucenaKarta);
+                    IgracevaRuka.Text += izvucenaKarta + "|";
+                    RukaIgraca.Add(izvucenaKarta);
+                }
+                else
+                {
+                    Console.WriteLine("Protivnikova karta " + izvucenaKarta);
+                    ProtivnikovaRuka.Text += izvucenaKarta + "|";
+                    RukaProtivnika.Add(izvucenaKarta);
+                }
+                Deck = tempDeck;
+                Console.WriteLine("Trenutno je u deku " + Deck.Count + " karata.");
+            }
+            Console.WriteLine("Metoda PocetnaRuka izvrsena.");
+        }
+        //Događaji
+        private void Igraj_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IgrajBlackJack)
+            {
+                if (Ulog.Text.Length < 1)
+                {
+                    MessageBox.Show("Niste ništa uložili.");
+                    return;
+                }
+                try
+                {
+                    ulozeniNovac = int.Parse(Ulog.Text);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Niste dobro unijeli novac.");
+                    return;
+                }
+                if (ulozeniNovac == 0)
+                {
+                    MessageBox.Show("Ulog ne može biti 0.");
+                    return;
+                }
+                OcistiRuke();
+                NapraviDeck();
+                PocetnaRuka();
+            }
+            else
+            {
+                MessageBox.Show("Igra je u tijeku");
+                return;
+            }
+        }
     }
+    
 }
---
+++ Casino/Karte.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    class Karte
+    {
+        //'\u2660', '\u2663', '\u2665', '\u2666'
+        //Pik, Tref, Srce, Karo
+        public List<char> Boje = new List<char> { '\u2660', '\u2663', '\u2665', '\u2666' };
+        public List<string> Brojevi = new List<string> { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+    }
+}
